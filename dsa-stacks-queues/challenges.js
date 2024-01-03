bd8b36b7a31ead855fc5fafe7df0589f6ed73f35
esthernickani <nickaniesther@gmail.com>
2024-01-03 16:16:47 -0700

    final solution

--- dsa-stacks-queues/challenges.js
+++ dsa-stacks-queues/challenges.js
@@ -30,29 +30,35 @@ const isBalanced = str => {
     let stack = new Stack();
     let arrOfChars = str.split('')
 
-    let balancedOrUnbalanced = null;
+    let balancedOrUnbalanced = 'Balanced';
     console.log(arrOfChars)
 
     arrOfChars.forEach(char => {
         if (openBrackets.includes(char)) {
             stack.push(char)
+            console.log(stack)
             
         } else if (closingBrackets.includes(char)) {
-            let item = stack.pop()
-            if (stack.isEmpty() || stack.peek() !== bracketPairs[item]) {
+            if (!stack.isEmpty() && stack.first.val !== bracketPairs[stack.first.val]) {
                 balancedOrUnbalanced = 'Not balanced'
+                return;
             }
+            stack.pop()
         }
     });
 
+    if (!stack.isEmpty()) {
+        balancedOrUnbalanced = 'Not balanced'
+    }
 
+    return balancedOrUnbalanced
         
     }
 
 
 
-//console.log(isBalanced('(hello'))
-//console.log(isBalanced('(nope]'))
-//console.log(isBalanced('(hi[there])'))
+console.log(isBalanced('(hello'))
+console.log(isBalanced('(nope]'))
+console.log(isBalanced('(hi[there])'))
 console.log(isBalanced('((ok)[nope)]'))
 
--- dsa-stacks-queues/find_survivor.js
+++ dsa-stacks-queues/find_survivor.js
@@ -1,110 +1,105 @@
 /** Node: node for a queue. */
-
 class Node {
     constructor(val) {
-      this.val = val;
-      this.next = null;
-    }
+        this.val = val;
+        this.next = null;
+      }
+  }
+    
+  class LinkedList {
+      constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+      }
+  
+      push(val, numOfPeople) {
+          let node = new Node(val);
+  
+          if (this.size === 0) {
+              this.first = node;
+              this.last = node;
+              this.size++;
+          } else if (this.size < numOfPeople - 1) {
+              this.last.next = node;
+              this.last = node;
+              this.size++;
+          } else {
+              this.last.next = node;
+              this.last = node;
+              this.size++;
+          }
+  
+          this.last.next = this.first;
+      }
+  
+      removeAtIdx(idx) {
+          let removedItem = this.getAt(idx);
+  
+          if (this.size === 1) {
+              this.first = null;
+              this.last = null;
+              this.size--;
+              return removedItem;
+          }
+  
+          if (idx === 0) {
+              this.first = this.first.next;
+              this.size--;
+              return removedItem;
+          }
+  
+          let prevNode = this.getAt(idx - 1);
+  
+          if (!removedItem || !prevNode || !prevNode.next) {
+              this.size--;
+              return;
+          }
+  
+          prevNode.next = removedItem.next;
+  
+          if (idx === this.size - 1) {
+              this.last = prevNode;
+          }
+  
+          this.size--;
+  
+          return removedItem;
+      }
+  
+      getAt(idx) {
+          let currentNode = this.first;
+          let counter = 0;
+  
+          while (counter < idx) {
+              currentNode = currentNode.next;
+              counter++;
+          }
+  
+          return currentNode;
+      }
   }
   
-  class LinkedList {
-    constructor() {
-      this.first = null;
-      this.last = null;
-      this.size = 0;
-    }
-
-    push(val, numOfPeople) {
-        let node = new Node(val)
-        if (this.size == 0) {
-            this.first = node;
-            this.last = node;
-            this.size ++;
-        } else if (this.size < numOfPeople-1) {
-            this.last.next = node;
-            this.last = node;
-            this.size++;
-        } else {
-            this.last.next = node;
-            this.last = node;
-            this.size++;
-        }
-        this.last.next = this.first
-        return;
-    }
-
-    getAt(idx) {
-        let currentNode = this.first;
-        let counter = 0;
-    
-        while (counter < idx) {
-          currentNode = currentNode.next;
-          counter ++;
-        }
-        return currentNode;
+  const findSurvivor = (numOfPeople, skip) => {
+      let newList = new LinkedList();
+  
+      for (let i = 1; i <= numOfPeople; i++) {
+          newList.push(i, numOfPeople);
       }
-
-    removeAtIdx(idx) {
-
-        let removedItem = this.getAt(idx)
-        console.log(this.list)
-        if (this.size === 0){
-            return null;
-        }
-    
-        if(this.size === 1){
-            removedItem = this.first;
-            this.first = null;
-            this.tail = null;
-            this.size --;
-            return removedItem;
-        }
-    
-        if(this.size === 2){
-            removedItem.next = this.first
-            this.first.next = null;
-            this.last = this.first
-            this.size --;
-            return removedItem;
-        }
-
-        if (idx === 0 & this.size >= 1){
-            removedItem = this.first;
-            this.first = this.first.next;
-            this.size--;
-            return removedItem;
-        }
-    
-        if (!removedItem || !removedItem.next){
-            this.size--;
-            return
-        }
-        removedItem.next = removedItem.next.next;
-        }
-    
-}
-
-const findSurvivor = (numOfPeople, skip) => {
-    newList = new LinkedList()
-    for (let i=1; i <= numOfPeople; i++) {
-        newList.push(i, numOfPeople)
-    }
-
-    
-    const Josephus = (linkedlist, start, skip) => {
-        if (linkedlist.size == 1) {
-            console.log(newList)
-            return newList.first.val
-        } 
-
-        start = (start + skip)%linkedlist.size
-        linkedlist.removeAtIdx(start)
-        return Josephus(linkedlist, start, skip)
-    }
-
-    return Josephus(newList, 0, skip)
-    
-
-}
-
-console.log(findSurvivor(10, 3))
+  
+      const Josephus = (linkedlist, start, skip) => {
+          if (linkedlist.size === 1) {
+              return newList.first.val;
+          }
+  
+          start = (start + skip - 1) % linkedlist.size;
+  
+          let survivor = linkedlist.removeAtIdx(start);
+  
+          return Josephus(linkedlist, start, skip);
+      };
+  
+      return Josephus(newList, 0, skip);
+  };
+  
+  console.log(findSurvivor(10, 3));  
